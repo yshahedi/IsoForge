5b7c53a08519f3071fe63c5f74bacf777af1d90b
yasser <yshahedi@gmail.com>
2026-06-07 14:56:23 +0330

    add client mode processor

--- processor.cc
+++ processor.cc
@@ -4,14 +4,194 @@
 
 using asio::ip::tcp;
 
-namespace Processor
+namespace YSH
 {
+    // -------------------------
+    // ClientSession
+    // -------------------------
+
+    ClientSession::ClientSession(asio::io_context &io_context, asio::thread_pool &worker_pool, std::shared_ptr<Config> config)
+        : socket_(io_context), io_context_(io_context), worker_pool_(worker_pool), config_(config), timer_(io_context), resolver_(io_context)
+    {
+        DL_ISO8583_DEFS_1993_GetHandler(&iso_handler_);
+    }
+
+    ClientSession::~ClientSession()
+    {
+    }
+
+    void ClientSession::start()
+    {
+    }
+
+    void ClientSession::send_async(std::unique_ptr<IsoMsg> request)
+    {
+        DL_UINT16 packedSize;
+        (void)DL_ISO8583_MSG_Pack(&iso_handler_, request->get(), body_, &packedSize);
+        body_len_ = packedSize;
+
+        auto self = shared_from_this();
+        set_timeout(std::chrono::milliseconds(config_->timeout));
+        /* auto endpoints = resolver_.resolve(config_->host,config_->port);
+         self->do_connect(endpoints);*/
+
+        resolver_.async_resolve(
+            config_->host,
+            std::to_string(config_->port),
+            [self](const asio::error_code &ec,
+                   tcp::resolver::results_type endpoints)
+            {
+                if (ec)
+                {
+                    self->finish(ec, nullptr);
+                    return;
+                }
+
+                self->do_connect(endpoints);
+            });
+    }
+
+    void ClientSession::finish(asio::error_code ec, std::unique_ptr<IsoMsg> response)
+    {
+        if (finished_)
+        {
+            return;
+        }
+
+        finished_ = true;
+
+        asio::error_code ignored;
+
+        timer_.cancel(ignored);
+        socket_.cancel(ignored);
+        socket_.close(ignored);
+
+        if (config_->handler)
+        {
+            config_->handler(ec, iso_handler_, std::move(response));
+        }
+    }
+
+    void ClientSession::do_connect(const tcp::resolver::results_type &endpoints)
+    {
+        auto self = shared_from_this();
+
+        asio::async_connect(
+            socket_,
+            endpoints,
+            [self](const asio::error_code &ec, const tcp::endpoint &)
+            {
+                if (ec)
+                {
+                    self->finish(ec, nullptr);
+                    return;
+                }
+
+                self->do_write();
+            });
+    }
+
+    void ClientSession::do_write()
+    {
+
+        auto self = shared_from_this();
+        auto len = body_len_;
+        uint8_t hdr[2];
+        hdr[0] = static_cast<uint8_t>((len >> 8) & 0xFF);
+        hdr[1] = static_cast<uint8_t>(len & 0xFF);
+
+        std::array<asio::const_buffer, 2> buffers = {
+            asio::buffer(hdr, 2),
+            asio::buffer(body_, len)};
+        asio::async_write(socket_, buffers,
+                          [this, self, len](std::error_code ec, std::size_t)
+                          {
+                              if (ec)
+                              {
+                                  self->finish(ec, nullptr);
+                                  return;
+                              }
+
+                              self->read_header();
+                          });
+    }
+
+    void ClientSession::read_header()
+    {
+        auto self = shared_from_this();
+        asio::async_read(socket_, asio::buffer(header_, 2),
+                         [this, self](std::error_code ec, std::size_t)
+                         {
+                             if (ec)
+                             {
+                                 self->finish(ec, nullptr);
+                                 return;
+                             }
+
+                             uint16_t len = (static_cast<uint16_t>(header_[0]) << 8) | header_[1];
+                             if (len == 0 || len > MAX_LEN)
+                             {
+                                 return;
+                             }
+
+                             read_body(len);
+                         });
+    }
+
+    void ClientSession::read_body(uint16_t len)
+    {
+        auto self = shared_from_this();
+
+        asio::async_read(socket_, asio::buffer(asio::buffer(body_, len)),
+                         [this, self, len](std::error_code ec, std::size_t length)
+                         {
+                             if (ec)
+                             {
+                                 self->finish(ec, nullptr);
+                             }
+                             else
+                             {
+                                 auto iso_msg = std::make_unique<IsoMsg>();
+                                 (void)DL_ISO8583_MSG_Unpack(&iso_handler_, body_, body_len_, iso_msg->get());
+                                 self->finish({}, std::move(iso_msg));
+                             }
+                         });
+    }
+
+    void ClientSession::set_timeout(std::chrono::milliseconds dur)
+    {
+        timeout_flg_ = false;
+        timer_.expires_after(dur);
+        timer_.async_wait([this, self = shared_from_this()](asio::error_code ec)
+                          {
+            
+            std::lock_guard<std::mutex> lock(mtx_);
+            if(timeout_flg_) return;
+            timeout_flg_=true;
+            if (!ec) 
+            {
+                auto iso_timeout_msg = std::make_unique<IsoMsg>();
+                self->finish(
+                    asio::error::make_error_code(asio::error::timed_out),
+                    std::move(iso_timeout_msg)
+                );
+
+            } 
+            else
+            {
+                self->finish(
+                    ec,
+                    nullptr
+                );
+            } });
+    }
+
     // -------------------------
     // Session
     // -------------------------
 
     Session::Session(tcp::socket socket, asio::io_context &io_context, asio::thread_pool &worker_pool, std::shared_ptr<Config> config)
-        : socket_(std::move(socket)), io_context_(io_context), worker_pool_(worker_pool),config_(config),timer_(socket_.get_executor()) 
+        : socket_(std::move(socket)), io_context_(io_context), worker_pool_(worker_pool), config_(config), timer_(socket_.get_executor())
     {
         DL_ISO8583_DEFS_1993_GetHandler(&iso_handler_);
     }
@@ -38,11 +218,11 @@ namespace Processor
                          [this, self](std::error_code ec, std::size_t)
                          {
                              if (ec)
-                                 return close();
+                                 return;
                              uint16_t len = (static_cast<uint16_t>(header_[0]) << 8) | header_[1];
                              if (len == 0 || len > MAX_LEN)
                              {
-                                 return close();
+                                 return;
                              }
 
                              read_body(len);
@@ -52,47 +232,40 @@ namespace Processor
     void Session::read_body(uint16_t len)
     {
         auto self = shared_from_this();
-        
+
         asio::async_read(socket_, asio::buffer(asio::buffer(body_, len)),
                          [this, self, len](std::error_code ec, std::size_t length)
                          {
                              if (ec)
-                                 return close();
+                                 return;
                              body_len_ = length;
                              set_timeout(std::chrono::milliseconds(config_->timeout));
-                             if (!config_->is_persist_connection)
-                             {
-                                 asio::post(self->worker_pool_, [self, this]()
-                                            { self->handle_message(body_, body_len_); });
-                             }
-                             else
-                             {
-                                 self->handle_message(body_, body_len_);
-                                 read_header();
-                             }
+                             asio::post(self->worker_pool_, [self, this]()
+                                        { self->handle_message(body_, body_len_); });
                          });
     }
 
     void Session::handle_message(const uint8_t *data, size_t len)
     {
+        auto self = shared_from_this();
+        DL_ISO8583_MSG iso_msg;
         DL_ISO8583_MSG_Init(NULL, 0, &iso_msg);
-
         (void)DL_ISO8583_MSG_Unpack(&iso_handler_, data, len, &iso_msg);
 
         config_->processor(iso_handler_, iso_msg);
         {
             timer_.cancel();
             std::lock_guard<std::mutex> lock(mtx_);
-            if(!timeout_flg_)
+            if (!timeout_flg_)
             {
-                timeout_flg_=true;
+                timeout_flg_ = true;
                 DL_UINT8 packBuf[MAX_LEN];
                 DL_UINT16 packedSize;
 
                 (void)DL_ISO8583_MSG_Pack(&iso_handler_, &iso_msg, packBuf, &packedSize);
 
                 asio::post(io_context_, [self = shared_from_this(), packBuf, packedSize]()
-                        { self->send_response(packBuf, packedSize); });
+                           { self->send_response(packBuf, packedSize); });
             }
             DL_ISO8583_MSG_Free(&iso_msg);
         }
@@ -101,25 +274,36 @@ namespace Processor
     void Session::send_response(const uint8_t *data, size_t len)
     {
         auto self = shared_from_this();
-        asio::async_write(socket_, asio::buffer(data, len),
+        uint8_t hdr[2];
+        hdr[0] = static_cast<uint8_t>((len >> 8) & 0xFF);
+        hdr[1] = static_cast<uint8_t>(len & 0xFF);
+
+        std::array<asio::const_buffer, 2> buffers = {
+            asio::buffer(hdr, 2),
+            asio::buffer(data, len)};
+        asio::async_write(socket_, buffers,
                           [this, self, data, len](std::error_code ec, std::size_t)
                           {
                               if (ec)
                                   return;
+
+                              if (config_->is_persist_connection)
+                                  read_header();
                           });
     }
 
     void Session::set_timeout(std::chrono::milliseconds dur)
     {
-        timeout_flg_=false;
+        timeout_flg_ = false;
         timer_.expires_after(dur);
-        timer_.async_wait([this,self = shared_from_this()](asio::error_code ec)
+        timer_.async_wait([this, self = shared_from_this()](asio::error_code ec)
                           {
             if (!ec) 
             {
                 std::lock_guard<std::mutex> lock(mtx_);
                 if(timeout_flg_) return;
                 timeout_flg_=true;
+                DL_ISO8583_MSG iso_timeout_msg;       
                 DL_ISO8583_MSG_Init(NULL, 0, &iso_timeout_msg);
 
                 (void)DL_ISO8583_MSG_Unpack(&iso_handler_, body_, body_len_, &iso_timeout_msg);
@@ -134,8 +318,7 @@ namespace Processor
                         { self->send_response(packBuf, packedSize); });
 
                 DL_ISO8583_MSG_Free(&iso_timeout_msg);
-            } 
-        });
+            } });
     }
 
     // -------------------------
@@ -163,54 +346,96 @@ namespace Processor
     }
 
     // -------------------------
+    // Client
+    // -------------------------
+
+    Client::Client(asio::io_context &io, std::shared_ptr<Config> config)
+        : io_(io), worker_pool_(config->worker_count), config_(config), workGuard_(asio::make_work_guard(io_)),
+          stopped_(false)
+    {
+    }
+
+    Client::~Client()
+    {
+        stop();
+    }
+
+    void Client::stop()
+    {
+        bool expected = false;
+
+        if (!stopped_.compare_exchange_strong(expected, true))
+        {
+            return;
+        }
+
+        workGuard_.reset();
+        io_.stop();
+    }
+
+    void Client::send(std::unique_ptr<IsoMsg> request)
+    {
+        if (stopped_.load())
+        {
+            throw std::runtime_error("Iso Client is stopped!");
+        }
+
+        asio::post(io_, [this, request = std::move(request)]() mutable
+                   { 
+                    std::make_shared<ClientSession>(io_, worker_pool_, config_)->send_async(std::move(request));
+                });
+    }
+
+    // -------------------------
     // Processor
     // -------------------------
     Processor::Processor()
     {
+
     }
     void Processor::stop()
     {
+
     }
-    void Processor::start(std::shared_ptr<Config> config)
+    void Processor::start(std::shared_ptr<Config> config,bool wait_flg)
     {
         config_ = config;
         try
         {
+            if (config_->is_server)
+            {
+                server_= std::make_unique<Server>(io_, config_);
+            }
+            else
+            {
+                client_= std::make_unique<Client>(io_, config_);
+            }
 
-            Server server(io_, config_);
             threads_.reserve(config_->thread_count);
             for (int i = 0; i < config_->thread_count; ++i)
             {
                 threads_.emplace_back([&]()
-                                      { io_.run(); });
+                                        { io_.run(); });
             }
-
-            for (auto &t : threads_)
-                t.join();
+            if(wait_flg)
+            {
+                for (auto &t : threads_)
+                    t.join();
+            }
+            else
+            {
+                for (auto &t : threads_)
+                    t.detach();
+            }           
         }
         catch (const std::exception &e)
         {
             std::cerr << "Fatal: " << e.what() << std::endl;
         }
-        
     }
 
-   
+    void Processor::send(std::unique_ptr<IsoMsg> request)
+    {
+        if(client_) client_->send(std::move(request));
+    }
 }
-
-
-/*const char *create_table = R"(CREATE TABLE card_profiles (
-                                        card_number INTEGER PRIMARY KEY,
-                                        balance REAL,
-                                        last_lat REAL,
-                                        last_lon REAL,
-                                        last_tx_time INTEGER,
-                                        status_code INTEGER,
-                                        daily_total REAL
-                                    ) WITHOUT ROWID;)";
-        for(int i=0;i<=999;i++)
-        {
-            std::string number = std::format("{:03}", i);
-            std::string uri = "file:card"+number+"?mode=memory&cache=shared";
-            create_db(number.c_str(),uri.c_str(),create_table);
-        }*/
--- processor.h
+++ processor.h
@@ -12,7 +12,7 @@
     # Kernel tuning
     sysctl -w net.core.somaxconn=65535
     sysctl -w net.ipv4.tcp_max_syn_backlog=65535
-   
+
  ****************************/
 
 #include <asio.hpp>
@@ -35,30 +35,92 @@
 
 using asio::ip::tcp;
 
-namespace Processor
+namespace YSH
 {
+    class IsoMsg
+    {
+        public:
+            IsoMsg()
+            {
+                DL_ISO8583_MSG_Init(NULL, 0, &raw);
+            }
+            ~IsoMsg()
+            {
+                DL_ISO8583_MSG_Free(&raw);
+            }
+            DL_ISO8583_MSG* get()
+            {
+                return &raw;
+            }
+
+        private:
+            DL_ISO8583_MSG raw;
+    };
+
 
     struct Config
     {
         Config(
-            uint32_t port=3000,
-            uint32_t thread_count=5,
-            uint32_t worker_count=20,
-            uint32_t timeout=2000,
-            bool is_persist_connection=false,
-            std::function<void(DL_ISO8583_HANDLER&,DL_ISO8583_MSG&)> processor =[](DL_ISO8583_HANDLER& isoHandler,DL_ISO8583_MSG& isoMsg){},
-            std::function<void(DL_ISO8583_HANDLER&,DL_ISO8583_MSG&)> timeout_processor=[](DL_ISO8583_HANDLER& isoHandler,DL_ISO8583_MSG& isoMsg){}
-        ):port(port),thread_count(thread_count),worker_count(worker_count),timeout(timeout),is_persist_connection(is_persist_connection),processor(std::move(processor)),timeout_processor(std::move(timeout_processor))
-        {
-
-        };
+            bool is_server = true,
+            uint32_t port = 3000,
+            std::string host = "127.0.0.1",
+            uint32_t thread_count = 5,
+            uint32_t worker_count = 20,
+            uint32_t timeout = 2000,
+            bool is_persist_connection = false,
+            std::function<void(DL_ISO8583_HANDLER &, DL_ISO8583_MSG &)> processor = [](DL_ISO8583_HANDLER &, DL_ISO8583_MSG &) {},
+            std::function<void(DL_ISO8583_HANDLER &, DL_ISO8583_MSG &)> timeout_processor = [](DL_ISO8583_HANDLER &, DL_ISO8583_MSG &) {},
+        std::function<void(const asio::error_code &ec, DL_ISO8583_HANDLER &, std::unique_ptr<IsoMsg>)> handler = [](const asio::error_code &ec, DL_ISO8583_HANDLER &, std::unique_ptr<IsoMsg>){}) 
+            : is_server(is_server),port(port), thread_count(thread_count), worker_count(worker_count), timeout(timeout), is_persist_connection(is_persist_connection), processor(std::move(processor)), timeout_processor(std::move(timeout_processor)),handler(std::move(handler)) {};
         uint32_t port;
         uint32_t thread_count;
         uint32_t worker_count;
         uint32_t timeout;
         bool is_persist_connection;
-        std::function<void(DL_ISO8583_HANDLER&,DL_ISO8583_MSG&)> processor;
-        std::function<void(DL_ISO8583_HANDLER&,DL_ISO8583_MSG&)> timeout_processor;
+        std::function<void(DL_ISO8583_HANDLER &, DL_ISO8583_MSG &)> processor;
+        std::function<void(DL_ISO8583_HANDLER &, DL_ISO8583_MSG &)> timeout_processor;
+        std::function<void(const asio::error_code &ec, DL_ISO8583_HANDLER &, std::unique_ptr<IsoMsg>)> handler ;
+        std::string host;
+        bool is_server;
+    };
+
+    // -------------------------
+    // ClientSession
+    // -------------------------
+    class ClientSession : public std::enable_shared_from_this<ClientSession>
+    {
+    public:
+        explicit ClientSession(asio::io_context &io_context, asio::thread_pool &worker_pool, std::shared_ptr<Config> config);
+
+        ~ClientSession();
+
+        void start();
+        void finish(asio::error_code ec, std::unique_ptr<IsoMsg>);
+        void send_async(std::unique_ptr<IsoMsg> response);
+
+    private:
+        void do_connect(const tcp::resolver::results_type &endpoints);
+        void do_write();
+        void read_header();
+        void read_body(uint16_t len);
+        void set_timeout(std::chrono::milliseconds dur);
+
+        tcp::socket socket_;
+        asio::steady_timer timer_;
+        tcp::resolver resolver_;
+
+        asio::io_context &io_context_;
+        asio::thread_pool &worker_pool_;
+
+        DL_ISO8583_HANDLER iso_handler_;
+
+        uint8_t header_[2];
+        uint8_t body_[MAX_LEN];
+        uint16_t body_len_;
+        std::shared_ptr<Config> config_;
+        bool timeout_flg_;
+        std::mutex mtx_;
+        bool finished_ = false;
     };
 
     // -------------------------
@@ -67,7 +129,7 @@ namespace Processor
     class Session : public std::enable_shared_from_this<Session>
     {
     public:
-        explicit Session(tcp::socket socket,asio::io_context& io_context, asio::thread_pool& worker_pool, std::shared_ptr<Config> config);
+        explicit Session(tcp::socket socket, asio::io_context &io_context, asio::thread_pool &worker_pool, std::shared_ptr<Config> config);
 
         ~Session();
 
@@ -84,12 +146,10 @@ namespace Processor
         tcp::socket socket_;
         asio::steady_timer timer_;
 
-        asio::io_context& io_context_;
-        asio::thread_pool& worker_pool_;
+        asio::io_context &io_context_;
+        asio::thread_pool &worker_pool_;
 
         DL_ISO8583_HANDLER iso_handler_;
-        DL_ISO8583_MSG iso_msg;
-        DL_ISO8583_MSG iso_timeout_msg;
 
         uint8_t header_[2];
         uint8_t body_[MAX_LEN];
@@ -97,7 +157,6 @@ namespace Processor
         std::shared_ptr<Config> config_;
         bool timeout_flg_;
         std::mutex mtx_;
-
     };
 
     // -------------------------
@@ -110,7 +169,7 @@ namespace Processor
 
     private:
         void do_accept();
-        
+
         asio::thread_pool worker_pool_;
         asio::io_context &io_;
         tcp::acceptor acceptor_;
@@ -118,21 +177,45 @@ namespace Processor
     };
 
     // -------------------------
+    // Client
+    // -------------------------
+    class Client
+    {
+    public:
+        Client(asio::io_context &io, std::shared_ptr<Config> config);
+        ~Client();
+        void stop();
+        void send(std::unique_ptr<IsoMsg> request);
+
+    private:
+        asio::thread_pool worker_pool_;
+        asio::io_context &io_;
+        std::shared_ptr<Config> config_;
+
+        using WorkGuard = asio::executor_work_guard<asio::io_context::executor_type>;
+
+        WorkGuard workGuard_;
+        std::atomic_bool stopped_;
+    };
+
+    // -------------------------
     // Processor
     // -------------------------
     class Processor
     {
-        public:
-            Processor();
-            void start(std::shared_ptr<Config> config);
-            void stop();
-        private:
-            std::vector<std::thread> threads_;
-            std::shared_ptr<Config> config_;
-            asio::io_context io_;
-    };
-
+    public:
+        Processor();
+        void start(std::shared_ptr<Config> config,bool wait_flg=true);
+        void stop();
+        void send(std::unique_ptr<IsoMsg> request);
    
+    private:
+        std::vector<std::thread> threads_;
+        std::shared_ptr<Config> config_;
+        asio::io_context io_;
+        std::unique_ptr<Server> server_;
+        std::unique_ptr<Client> client_;
+    };
 
 }
 
